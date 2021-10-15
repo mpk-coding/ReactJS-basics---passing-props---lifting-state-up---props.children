0247a753ae7d0ca5a4a4aa832f02bfc604300bd6
mpk-coding <thamak@gmail.com>
2021-10-15 18:59:04 +0200

    reset inputs on form submission

--- src/components/AddUser/AddUserForm.js
+++ src/components/AddUser/AddUserForm.js
@@ -4,18 +4,36 @@ import Button from "../Button/Button.js";
 import "./AddUserForm.css";
 
 const defaultAge = 18;
+const defaultState = {id: Number(), name: "", age: defaultAge};
 
 const AddUserForm = (props) => {
-  const [formData, setFormData] = useState({
-    id: Number(),
-    name: "",
-    age: defaultAge,
-  });
+  const [formData, setFormData] = useState(defaultState);
   //
   const submitHandler = (event) => {
     event.preventDefault();
     //
     props.onSubmit(formData);
+    //
+    resetInputs(event);
+    //
+  };
+
+  const resetInputs = (event) => {
+    for (const element of event.target) {
+      switch (element.getAttribute("data-key")) {
+        case "name":
+          element.value = defaultState.name;
+          break;
+        case "age":
+          element.value = defaultState.age;
+          break;
+        default:
+          break;
+      }
+    }
+    //
+    setFormData(defaultState);
+    //
   };
 
   const changeHandler = (event) => {
